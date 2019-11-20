7a6d8f717d74088bb66bf04e33a81110d74bf176
normanlai <lai.xinming@gmail.com>
2019-11-20 09:13:43 +0800

    did authentication for all user routes and logout for user dashboard

--- leave-database/controllers/user.js
+++ leave-database/controllers/user.js
@@ -5,27 +5,35 @@ const Leave = require("../models/leave.js");
 
 //user - leave application form
 user.get("/user/:id/new", (req, res) => {
-  Employee.findById(req.params.id, (err, foundEmployee) => {
-    res.render("./user/new.ejs", {
-      employee: foundEmployee
+  if (req.session.currentUser) {
+    Employee.findById(req.params.id, (err, foundEmployee) => {
+      res.render("./user/new.ejs", {
+        employee: foundEmployee
+      });
     });
-  });
+  } else {
+    res.redirect("/sessions/new");
+  }
 });
 
 //user - user dashboard
 user.get("/user/:id", (req, res) => {
-  Employee.findById(req.params.id, (err, foundEmployee) => {
-    Leave.find({ employeeId: req.params.id })
-      .populate("employeeId")
-      .exec(function(err, foundLeave) {
-        if (err) return handleError(err);
-        console.log(foundLeave);
-        res.render("./user/index.ejs", {
-          employee: foundEmployee,
-          leave: foundLeave
+  if (req.session.currentUser) {
+    Employee.findById(req.params.id, (err, foundEmployee) => {
+      Leave.find({ employeeId: req.params.id })
+        .populate("employeeId")
+        .exec(function(err, foundLeave) {
+          if (err) return handleError(err);
+          console.log(foundLeave);
+          res.render("./user/index.ejs", {
+            employee: foundEmployee,
+            leave: foundLeave
+          });
         });
-      });
-  });
+    });
+  } else {
+    res.redirect("sessions/new");
+  }
 });
 //user - create leave application
 user.post("/user/:id", (req, res) => {
